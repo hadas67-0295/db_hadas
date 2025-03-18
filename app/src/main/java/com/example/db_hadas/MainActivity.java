e8e063b274cc89d4db6ec377ccc40f4fc88d493d
hadas <hadasabu67@gmail.com>
2025-03-18 13:59:24 +0200

    Jest add the MainActivity.java

---
+++ app/src/main/java/com/example/db_hadas/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.db_hadas;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etFirstName = findViewById(R.id.etFirstName);
+    EditText etLastName = findViewById(R.id.etLastName);
+    EditText etAddress = findViewById(R.id.etAddress);
+    EditText etPhone = findViewById(R.id.etPhone);
+
+    button btnSave = findViewById(R.id.btnSave);
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
